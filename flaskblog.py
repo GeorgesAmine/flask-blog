bbbdcddecd3e43e6e0a50bca4a0c468139507997
GeorgesAmine <georgesamine94@gmail.com>
2022-08-14 15:51:23 +0300

    Added database model to flaskblog.py file and create site.db file

--- flaskblog.py
+++ flaskblog.py
@@ -3,13 +3,43 @@ This is the main app file
 '''
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
 import config 
 
 # Instanciating flass app
 app = Flask(__name__)
-
 # Adding secret key to app configuration
 app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+# Creating database
+db = SQLAlchemy(app)
+
+# Creating User models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+# Creating Post models
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text(20), nullable=False)
+    user_id =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 # Creating dummy posts to be replaced by database requests later
 posts = [
