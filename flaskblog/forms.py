bf70ffaef9426a2e68ff43fba56db79378ab6369
GeorgesAmine <georgesamine94@gmail.com>
2022-08-16 12:15:56 +0300

    Profile info and picture update on account page

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -5,9 +5,11 @@ This module is then imported to the main app file in order to
 create instances of froms to be passed and rendered in html pages
 '''
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 # RgistrationForm class that inherits from FlaskForm
 class RegistrationForm(FlaskForm):
@@ -37,3 +39,26 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+# UpdateAccountForm
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture',
+                         validators=[FileAllowed(['jpg','png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose another one')
